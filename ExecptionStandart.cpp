78e60e7439a079a1ce4af55f8a77835647e16b85
Evelofdex <evelofdex@gmail.com>
2026-06-02 17:35:34 +0700

    deklarasi array data

--- ExecptionStandart.cpp
+++ ExecptionStandart.cpp
@@ -6,6 +6,10 @@ using namespace std;
 
 int main(){
     cout << "Awal program" << endl;
+    try{
+        // deklarasi array data dengan panjang index 3
+        array <int, 3> data = {1, 2, 3};
+    }
 }
 
 
